3c055ab4700123f0f9e4097eeb69865752c56a3a
SjorsMaster <sjorspnk@gmail.com>
2021-03-10 21:04:04 +0100

    Added namespace upon request and fixed some small issues with the clouds

--- Assets/Scripts/Overworld/CloudBehaviour.cs
+++ Assets/Scripts/Overworld/CloudBehaviour.cs
@@ -9,28 +9,31 @@ using Beweegmaatje.Variables;
 /// This is done with a lerp, and customisable variables for the developer to play with such as boundaries, directions, speed, etc.
 /// It uses the Beweegmaatje Variables to obtain the stepcount.
 /// </summary>
-public class CloudBehaviour : MonoBehaviour
+namespace Overworld
 {
-    [SerializeField] IntVariable _steps;
-    public int _startStep;
-    [SerializeField] Vector3 _resetTriggers, _spawnPosition, _direction;
-    [SerializeField] Vector2 _speed;
-
-    private void Start()
+    public class CloudBehaviour : MonoBehaviour
     {
-        _startStep = -_steps + (int)transform.position.z;
-    }
+        [SerializeField] IntVariable _steps;
+        public int _startStep;
+        [SerializeField] Vector3 _resetTriggers, _spawnPosition, _direction;
+        [SerializeField] Vector2 _speed;
 
-    void FixedUpdate()
-    {
-        transform.position += _direction * _speed.x;
-        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y, -(_steps - _startStep)), _speed.y);
-        if (transform.position.x >= _resetTriggers.x || transform.position.z <= _resetTriggers.z) Reset();
-    }
+        private void Start()
+        {
+            _startStep = -_steps + (int)transform.position.z;
+        }
 
-    void Reset()
-    {
-        transform.position = _spawnPosition;
-        _startStep = _steps + (int)transform.position.z;
+        void FixedUpdate()
+        {
+            transform.position += _direction * _speed.x;
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y, -(_steps - _startStep)), _speed.y);
+            if (transform.position.x >= _resetTriggers.x || transform.position.z <= _resetTriggers.z) Reset();
+        }
+
+        void Reset()
+        {
+            transform.position = _spawnPosition;
+            _startStep = _steps + (int)transform.position.z;
+        }
     }
 }
